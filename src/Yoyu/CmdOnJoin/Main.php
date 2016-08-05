e72dab443d585bbb04edeffdc107e1474eafb06c
Hua华noob code! xD <ronghua00000@gmail.com>
2016-08-05 06:24:08 -0500

    lol

--- src/Yoyu/CmdOnJoin/Main.php
+++ src/Yoyu/CmdOnJoin/Main.php
@@ -1,1 +1,12 @@
+<?php
 
+namespace Yoyu\CmdOnJoin;
+
+use pocketmine\command\ConsoleCommandSender;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\player\PlayerRespawnEvent;
+use pocketmine\event\player\PlayerQuitEvent;
